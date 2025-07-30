14c60420cc0642985fa60e3e921504a830940df4
mahmoudnader150 <mahnader222@gmail.com>
2025-07-30 14:39:13 +0300

    Adding state to app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./styles.css"
+import { useState } from "react";
+
 export default function App() {
+  const [newItem , setNewItem] = useState("");
+
   return(
   <>
   <form className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item" />
-      <button className="btn">Add</button>
+      <input 
+        value={newItem} 
+        onChange={e => setNewItem(e.target.value)}
+        type="text" 
+        id="item" />
     </div>
+    <button className="btn">Add</button>
+   
   </form>
   <h1 className="header">Your Tasks </h1>
   <br />
